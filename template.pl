0eea474296d98c68f04c60561351e2e65d0a4c4f
James S White <james@jameswhite.org>
2011-01-12 16:05:00 -0600

    adding some sql code to aggregate logs

--- template.pl
+++ template.pl
@@ -5,14 +5,67 @@ BEGIN {
       }
 
 use strict;
+use DBI;
+use DBD::SQLite;
 use Data::Dumper;
 use Template::Regex;
+my $DBFILE="/var/tmp/connections.db"
+
+# my $db = DBI->connect("dbi:SQLite:$DBFILE", "", "", {RaiseError => 1, AutoCommit => 1});
+# $db->do("CREATE TABLE IF NOT EXISTS hosts (host_id INTEGER PRIMARY KEY AUTOINCREMENT, ipaddress VARCHAR(15), zone VARCHAR(32))");
+# $db->do("CREATE TABLE IF NOT EXISTS connections (source_host_id INTEGER, destination_host_id INTEGER, source_port INTEGER, destination_port INTEGER, protocol VARCHAR(8), count INTEGER)");
 
 my $tr = new Template::Regex;
 $tr->load_template_file("cisco-asa.yml");
 while(my $line=<STDIN>){
     chomp($line);
+    my ($proto, $src_raw, $src_zone, $src_ip, $src_port, $tgt_raw, $tgt_zone, $tgt_ip, $tgt_port);
     my $result = $tr->parse_line($line);
-    print Data::Dumper->Dump([$result]);
+    if ($result->{'name'} eq 'cisco_asa.session_buildup'){
+        $proto = $result->{'pattern'}->[6];
+        $proto=~tr/A-Z/a-z/;
+        $src_raw = $result->{'pattern'}->[8];
+        if($src_raw=~m/([^:])+:([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\/([0-9]+)\s+\(([^\)]*)\)/){
+            $src_zone = $1;
+            $src_ip   = $2;
+            $src_port = $3;
+        }
+        $tgt_raw = $result->{'pattern'}->[9];
+        if($src_raw=~m/([^:])+:([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\/([0-9]+)\s+\(([^\)]*)\)/){
+            $tgt_zone = $1;
+            $tgt_ip   = $2;
+            $tgt_port = $3;
+        }
+        print "[$proto] $src_zone:$src_ip/$src_port -> $tgt_zone:$tgt_ip/$tgt_port\n";
+   }
+#  if($proto&&$src_zone&&$src_ip&&$src_port&&$tgt_zone&&$tgt_ip&&$tgt_port){
+#        $db->do("INSERT INTO hosts (ipaddress,zone) SELECT '$src_ip','$src_zone' WHERE NOT EXISTS (SELECT * FROM hosts WHERE ipaddress = '$src_ip')");
+#        $db->do("INSERT INTO hosts (ipaddress,zone) SELECT '$tgt_ip','$tgt_zone' WHERE NOT EXISTS (SELECT * FROM hosts WHERE ipaddress = '$tgt_ip')");
+#         my $src_id_arry = $db->selectall_arrayref("SELECT host_id from hosts WHERE ipaddress='$src_ip'");
+#         my $tgt_id_arry = $db->selectall_arrayref("SELECT host_id from hosts WHERE ipaddress='$tgt_ip'");
+#         #print $#{ $src_id }." -> ".$#{ $tgt_id }."\n";
+#         my $src_id = $src_id_arry->[0]->[0];
+#         my $tgt_id = $tgt_id_arry->[0]->[0];
+#         my $tally = $db->selectall_arrayref("SELECT count FROM connections WHERE source_host_id='$src_id' AND destination_host_id='$tgt_id' AND source_port='$src_port' AND destination_port='$tgt_port' AND protocol='$proto'");
+#         if($#{ $tally } == -1){
+#             #print "Inserting\n";
+#             $db->do("INSERT INTO connections (source_host_id,destination_host_id,source_port,destination_port,protocol,count) VALUES ('$src_id','$tgt_id','$src_port','$tgt_port','$proto','1')");
+#         }else{
+#             #print "Updating\n";
+#             my $newcount=$tally->[0]->[0] + 1;
+#             $db->do("UPDATE connections SET count='$newcount' WHERE  source_host_id='$src_id' AND destination_host_id='$tgt_id' AND source_port='$src_port' AND destination_port='$tgt_port' AND protocol='$proto'");
+}
+
+
+
+
+
+
+
+
+
+
+
+
 }
 exit 1;
