d6c50a02089fe04e9a4b4726b7476fdffcf219f4
jameswhite <jameswhite@eftsource.com>
2011-08-04 12:49:11 -0500

    better libdir locator

--- taillog.pl
+++ taillog.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -wT
+#!/usr/bin/perl -w
 ################################################################################
 BEGIN {
         # figure out where we are and include our relative lib directory
