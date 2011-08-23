27395541efb9ae69af3a444b32b7a62db8533034
jameswhite <jameswhite@eftsource.com>
2011-08-23 12:32:31 -0500

    tweaking events

--- logwatch.pl
+++ logwatch.pl
@@ -152,7 +152,7 @@ sub sketch_connection {
 
         #next if ( $args->[3] =~ m/^arctic/) ; # ignore the lab
 
-        $kernel->yield('send_sketch', "Job: $args->[10]: ($args->[3] printing to $args->[7])");
+        $kernel->yield('send_sketch', "Job: $args->[10]: [ $args->[3] -> $args->[7] ]");
         $heap->{'pending'}->{ $args->[10] } = 1;
         $kernel->delay('event_timeout', 10, $args->[10],"job timed out");
     }elsif ($match eq 'windows_event.print_end'){
