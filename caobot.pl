6c7c5ebfada1f1f85639b6f5fd99fe66d352044a
jameswhite <jameswhite@eftsource.com>
2011-08-23 16:54:03 -0500

    more ldap work

--- caobot.pl
+++ caobot.pl
@@ -183,7 +183,7 @@ sub printer_lookup{
     my $ldap = Net::LDAP->new( "ldap.$domainname" ) or warn "$@\n";
     my $mesg = $ldap->bind;
     print STDERR $mesg->error."\n" if $mesg->code;
-    $mesg = $ldap->search( base   => "ou=Card@Once,$basedn", filter => "(uniqueMember=cn=$soekris,ou=Hosts,$basedn)");
+    $mesg = $ldap->search( base   => "ou=Card\@Once,$basedn", filter => "(uniqueMember=cn=$soekris,ou=Hosts,$basedn)");
     print STDERR $mesg->error."\n" if $mesg->code;
     foreach $entry ($mesg->entries) { $entry->dump; }
     print "$soekris, $replyto, $who\n";
