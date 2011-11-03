6f7350c64e0d99bda883de6f5705686179e9c3bb
James S White <james@jameswhite.org>
2011-11-03 13:50:46 -0500

    process spawning to caobot

--- caobot.pl
+++ caobot.pl
@@ -340,6 +340,7 @@ sub irc_public {
             $sanitized_device='skrs';
         }
 print Data::Dumper->Dump([{'sanitized_device_1' => $sanitized_device}]);
+print STDERR "device: $device\n";
         if($number = $device=~m/([0-9]+)/){
 print STDERR "NUMBER: $number\n";
             if($number < 10){ $number="000$number"; }
