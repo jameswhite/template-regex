cef8b9d767bb938bd3a4f317814d7008155534cf
James S White <jameswhite@eftsource.com>
2012-08-08 12:52:20 -0500

    add/remove/show watch list

--- caobot.pl
+++ caobot.pl
@@ -167,7 +167,7 @@ sub watchlist{
 sub run_watchlist{
    my ($self, $kernel, $heap, $sender, @args) = @_[OBJECT, KERNEL, HEAP, SENDER, ARG0 .. $#_];
    foreach my $device (@{ $heap->{'watchlist'} }){
-        $kernel->yield('spawn', ["rtatiem",$self->sanitize($device)]);
+        $kernel->yield('spawn', ["rtatiem_curt",$self->sanitize($device)]);
    }
    # run the watch list in 30
    $kernel->delay('run_watchlist',30);
